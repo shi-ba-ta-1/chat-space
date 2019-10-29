cbb5d98cb8d2ebc3cf06ad8588e00aac523603c5
shi-ba-ta-1 <56523540+shi-ba-ta-1@users.noreply.github.com>
2019-10-29 20:28:06 +0900

    修正しました！

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -48,7 +48,6 @@ $(document).on('turbolinks:load', function(){
   var reloadMessages = function () {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       var last_message_id = $('.message:last').data("message-id");
-      console.log(last_message_id)
 
       $.ajax({
         url: "api/messages",
@@ -57,7 +56,6 @@ $(document).on('turbolinks:load', function(){
         data: {id: last_message_id}
       })
       .done(function (messages) {
-        console.log(messages)
         var insertHTML = '';
         messages.forEach(function (message) {
           insertHTML = buildHTML(message);
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,12 +1,3 @@
-# json.message do
-#   json.content  @message.content
-#   json.id  @message.id
-#   json.user_name  @message.user.name
-#   json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
-#   json.image @message.image.url
-#   json.(@message, :content, :image)
-# end
-
 json.(@message, :content, :image)
 json.user_name @message.user.name
 json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
