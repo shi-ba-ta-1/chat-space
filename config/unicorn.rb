a3f4168e4ff5651c56ed176410dfbf4b64ab17f6
shi-ba-ta-1 <56523540+shi-ba-ta-1@users.noreply.github.com>
2019-11-02 11:55:31 +0900

    a

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+# ../が一つ増えている
 app_path = File.expand_path('../../../', __FILE__)
 
-#アプリケーションサーバの性能を決定する
 worker_processes 1
+# currentを指定
+working_directory "#{app_path}/current"
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+# それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
